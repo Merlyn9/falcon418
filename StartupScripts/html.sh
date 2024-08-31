482290b09bd1140a117879c70e26da94f0bc7e0d
Merlyn <merlyndevelops@gmail.com>
2024-08-31 16:18:24 -0400

    New file upload
    Bash your way into web development.

---
+++ StartupScripts/html.sh
@@ -0,0 +1,14 @@
+!# /bin/bash
+
+echo "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Me</title>
+</head>
+<body>
+    
+</body>
+</html>" > file.html 
+
