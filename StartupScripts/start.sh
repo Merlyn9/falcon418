7569741ffc7ed3449f292de22264542d2df97b52
Merlyn <merlyndevelops@gmail.com>
2024-08-31 14:56:23 -0400

    edig

--- StartupScripts/start.sh
+++ StartupScripts/start.sh
@@ -1,5 +1,9 @@
 !# /bin/bash
 
+
+#In bash, the # edits out in green, comments you can add to your code.
+#It is imperative that you # or comment out every line of code that 
+#is a comment so you can add context to a problems in a section of code.
 sudo apt-get update && upgrade -y
 sudo apt install htop net-tools macchanger -y
 sudo apt install gnome-boxes -y
