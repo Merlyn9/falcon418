9f2d67295e9726fe64a679f7353278bba2f5b80c
Merlyn <merlyndevelops@gmail.com>
2024-08-31 16:30:51 -0400

    edit html.sh

--- StartupScripts/html.sh
+++ StartupScripts/html.sh
@@ -12,7 +12,7 @@ echo "<!DOCTYPE html>
 <h1>Instant Deployment</h1>
 <h3>You change the script and run it, out pops a baby.</h3>
 <br>
-<p>EVerybody is different, you can spin up forms, you can spin up temporary blogs.  YOu must maintain the script and 
+<p>Everybody is different, you can spin up forms, you can spin up temporary blogs.  YOu must maintain the script and 
 adapt to change.  Change is good. Learning is great</p>
 #Below this comment is the end, where the echo gets wrote to a new file. 
 
