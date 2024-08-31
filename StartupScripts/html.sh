001149526617b546ce7c3a793275267add46d20b
Merlyn <merlyndevelops@gmail.com>
2024-08-31 16:27:26 -0400

    edit file

--- StartupScripts/html.sh
+++ StartupScripts/html.sh
@@ -5,9 +5,15 @@ echo "<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Change Me</title>
+    <title>Instant Deployment</title>
 </head>
 <body>
+<center>
+<h1>Instant Deployment</h1>
+<h3>You change the script and run it, out pops a baby.</h3>
+<br>
+<p>EVerybody is different, you can spin up forms, you can spin up temporary blogs.  YOu must maintain the script and 
+adapt to change.  Change is good. Learning is great</p>
 #Below this comment is the end, where the echo gets wrote to a new file. 
 
 #Change the script for a different webpage for instant future spin up.
