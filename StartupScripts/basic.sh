fb6f7df3f0a61c59a22435318a74ffd1318cc027
Merlyn <merlyndevelops@gmail.com>
2024-08-31 15:49:59 -0400

    new file

--- StartupScripts/basic.sh
+++ StartupScripts/basic.sh
@@ -1,1 +1,10 @@
 !# /bin/bash
+
+# Ctrl - C to exit cmatrix screensaver
+
+sudo apt-get install net-tools -y
+sudo apt-get install lynis -y
+sudo apt-get install rkhunter -y
+sudo apt install python3-venv python3-pip -y
+sudo apt-get instal cmatrix -y
+cmatrix
