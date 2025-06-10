8fb7e7f928422887e70a0afe047f7f65f686f88e
Merlyn <merlyndevelops@gmail.com>
2025-06-10 18:25:00 -0400

    weather maps
    run this program for weather maps in an instant.
    
    Firefox for Linux

---
+++ weather.sh
@@ -0,0 +1,5 @@
+!#bin/bash
+
+
+#Refresh the outages tab after loading program.
+firefox "https://radar.weather.gov/station/KCAE/standard" "https://radar.weather.gov/region/southeast/standard" "https://radar.weather.gov/" "https://graphical.weather.gov/sectors/midatlantic.php?element=MaxT" "https://digital.weather.gov/" "https://www.star.nesdis.noaa.gov/GOES/conus.php?sat=G19&refresh=true" "https://poweroutage.us/area/state/south%20carolina" 
